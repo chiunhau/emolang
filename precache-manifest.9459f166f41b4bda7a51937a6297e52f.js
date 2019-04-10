44c0246bb33a6a925a6b9cb225e50f3e013ff988
Chiunhau <chiunhau9@gmail.com>
2019-04-10 21:26:56 +0300

    Updates

---
+++ precache-manifest.9459f166f41b4bda7a51937a6297e52f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/emolang/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d12487873ec253232b6c",
+    "url": "/emolang/static/js/runtime~main.786738dd.js"
+  },
+  {
+    "revision": "d75ef9436b293cf4a530",
+    "url": "/emolang/static/js/main.5f07d052.chunk.js"
+  },
+  {
+    "revision": "2f5f4255dcce157b4e92",
+    "url": "/emolang/static/js/2.91f9638c.chunk.js"
+  },
+  {
+    "revision": "d75ef9436b293cf4a530",
+    "url": "/emolang/static/css/main.faa9ad91.chunk.css"
+  },
+  {
+    "revision": "619a2f9ff445ce3b427f6d6354dd16c3",
+    "url": "/emolang/index.html"
+  }
+];
